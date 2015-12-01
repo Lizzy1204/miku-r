d8135bc4ab8781103608389c587c836774e1811d
MIKUScallion <MIKUScallion@hotmail.com>
2015-12-01 14:34:01 +0800

    add miku-r

--- php/post.php
+++ php/post.php
@@ -11,9 +11,9 @@
     //垮域名头
     header('Access-Control-Allow-Origin: *');
     //设置图片输出头
-    //header("Content-Type: image/jpeg;text/html; charset=utf-8");
+    header("Content-Type: image/jpeg;text/html; charset=utf-8");
     //设置文本输出
-    header("Content-Type:text/html;charset=utf-8");
+    //header("Content-Type:text/html;charset=utf-8");
     //基础路径
     //$basepath = "/var/www/html/myweb/miku-r";//本地版
     $basepath = "/var/www/html/miku-r";//服务器版
